66e0b2a48d81dbe1770e11e46e5afe56d47aec07
Mauro  Avellaneda <mauroeavellaneda@gmail.com>
2020-08-17 21:53:39 +0200

    modifying port in package.json, app.js and in userCanCreateAContact

--- cypress/integration/userCanCreateAContact.feature.js
+++ cypress/integration/userCanCreateAContact.feature.js
@@ -1,10 +1,10 @@
 describe('user can create a contact', () => {
-    it('test', () => {
-        cy.visit('http://localhost:3001')
+    beforeEach('test', () => {
+        cy.visit('http://localhost:3474')
         cy.get('#add-contact').click()
         cy.get('#name').type('Mauro')
 		cy.get('#email').type('mauroeavellaneda@gmail.com')
-		cy.get('#phone').type('0700 1010100764040738')
+		cy.get('#phone').type('0764040738')
 		cy.get('#company').type('Craft Academy')
 		cy.get('#notes').type('Awesome coder')
         cy.get('#twitter').type('@mauro_avellaneda')
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -22,3 +22,43 @@ const  renderContacts = () => {
         div.innerHTML = '<p>You have no contacts in your address book yet</p>'
       }
   }
+  document.addEventListener('DOMContentLoaded', () => {
+	renderContacts()
+    const  contactForm = document.getElementById('new-contact-form')
+    const  toggleFormVisibilityButton = document.getElementById('add-contact')
+	contactForm.style.display = 'none'
+ 
+	toggleFormVisibilityButton.addEventListener('click', () => {
+		if (contactForm.style.display === '') {
+			contactForm.style.display = 'none'
+		} else {
+			contactForm.style.display = ''
+		}
+	})
+	contactForm.addEventListener('submit', event  => {
+		event.preventDefault()
+
+		// 1. Read all the input fields and get their values
+		const { name, email, phone, company, notes, twitter } = contactForm.elements
+
+		const  contact = {
+			name:  name.value,
+			email:  email.value,
+			phone:  phone.value,
+			company:  company.value,
+			notes:  notes.value,
+			twitter:  twitter.value,
+		}
+
+		console.log(contact)
+
+		let  contacts = JSON.parse(storage.getItem('contacts')) || []
+
+		contacts.push(contact)
+
+		// 2. Save them to our storage
+		storage.setItem('contacts', JSON.stringify(contacts))
+		renderContacts()
+		contactForm.reset()
+   })
+})
